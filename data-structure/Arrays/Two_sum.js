5e08e6d88a7d5d141b725475f0fa9203a53e4406
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-10-26 23:24:08 +0600

    Two sum - this is not the better solution

---
+++ data-structure/Arrays/Two_sum.js
@@ -0,0 +1,14 @@
+// * this is not the better solution, make it better in upcoming code
+
+const twoSum = (nums,target) =>{
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i,j]
+            }
+        }
+    }
+    return []
+}
+
+console.log(twoSum([3,6,7,2],9))
